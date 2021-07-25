b0e74ff1997aa6e50c904fccfa8428848fe10937
Sahej Kaur Makkar <sahejk@fb.com>
2021-07-25 19:45:02 +0800

    Added stepper in checkout page

--- src/screens/checkout/Checkout.js
+++ src/screens/checkout/Checkout.js
@@ -1,13 +1,120 @@
 import React, { Component } from 'react';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import StepContent from '@material-ui/core/StepContent';
+import { withStyles, Button, } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 import Header from '../../common/header/Header';
 
-class Checkout extends Component{
+const styles = (theme => ({
 
-    render(){
-        return(
-            <Header baseUrl={this.props.baseUrl} showHeaderSearchBox={false}/>
+    actionsContainer: {
+        marginBottom: theme.spacing(2),
+    },
+    button: {
+        marginTop: theme.spacing(1),
+        marginRight: theme.spacing(1),
+    },
+    resetContainer: {
+        padding: theme.spacing(3),
+    },
+
+
+}))
+
+
+
+class Checkout extends Component {
+    constructor() {
+        super()
+        this.state = {
+            activeStep: 0,
+            steps: this.getSteps(),
+
+        }
+    }
+    getSteps = () => {
+        return ['Delivery', 'Payment'];
+    }
+    nextButtonClickHandler = () => {
+        let activeStep = this.state.activeStep;
+        activeStep++;
+        this.setState({
+            ...this.state,
+            activeStep: activeStep,
+        });
+    }
+
+    backButtonClickHandler = () => {
+        let activeStep = this.state.activeStep;
+        activeStep--;
+        this.setState({
+            ...this.state,
+            activeStep: activeStep,
+        });
+    }
+
+    changeButtonClickHandler = () => {
+        this.setState({
+            ...this.state,
+            activeStep: 0,
+        });
+    }
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <div>
+                <Header baseUrl={this.props.baseUrl} showHeaderSearchBox={false} />
+                <div className="flex-container">
+                    <div className="stepper-container">
+                        <Stepper activeStep={this.state.activeStep} orientation="vertical">
+                            {this.state.steps.map((label, index) => (
+                                <Step key={label}>
+                                    <StepLabel>{label}</StepLabel>
+                                    <StepContent>
+                                        <Typography>dummy Address</Typography>
+                                        <div className={classes.actionsContainer}>
+                                            <div>
+                                                <Button
+                                                    disabled={this.state.activeStep === 0}
+                                                    onClick={this.backButtonClickHandler}
+                                                    className={classes.Button}
+                                                >
+                                                    Back
+                                                </Button>
+                                                <Button
+                                                    variant="contained"
+                                                    color="primary"
+                                                    onClick={this.nextButtonClickHandler}
+                                                    className={classes.button}
+                                                >
+                                                    {this.state.activeStep === this.state.steps.length - 1 ? 'Finish' : 'Next'}
+                                                </Button>
+                                            </div>
+                                        </div>
+                                    </StepContent>
+                                </Step>
+                            ))}
+                        </Stepper>
+                        {this.state.activeStep === this.state.steps.length && (
+                            <Paper square elevation={0} className={classes.resetContainer}>
+                                <Typography>View the summary and place your order now!</Typography>
+                                <Button onClick={this.changeButtonClickHandler} className={classes.button}>
+                                    Change
+                                </Button>
+                            </Paper>
+                        )}
+
+                    </div>
+                </div>
+
+
+            </div>
         )
     }
 }
 
-export default Checkout;
+export default withStyles(styles)(Checkout);
