46c85840082d5693e515c4045afb7a480c69fc65
Sahej Kaur Makkar <sahejk@fb.com>
2021-07-25 18:25:22 +0800

    Add details page

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
 import {BrowserRouter as Router,Route} from 'react-router-dom';
 import Home from './home/Home';
+import Details from './details/Details';
 
 
 
@@ -15,7 +16,8 @@ class Controller extends Component{
         return(
             <Router>
                 <div className = 'main-conatiner'>
-                    <Route exact path = '/' render={(props) => <Home {...props} baseUrl = {this.baseUrl}/>}/> {/* Route to home Page */ }
+                    <Route exact path = '/' render={(props) => <Home {...props} baseUrl = {this.baseUrl}/>}/>
+                    <Route path='/restaurant/:id' render={(props) => <Details {...props} baseUrl={this.baseUrl} />} />
                 </div>
             </Router>
 
---
+++ src/screens/details/Details.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Header from '../../common/header/Header';
+
+class Details extends Component{
+    render(){
+        return(
+            <div>
+                <Header></Header>
+                <h1>This is Details Page</h1>
+            </div>
+        )
+    }
+}
+
+export default Details;
